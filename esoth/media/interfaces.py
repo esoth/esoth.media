ffc6d157d946d3d9c3469db678d490803f6bb5d1
Esoth <oolitegroove@gmail.com>
2016-10-23 10:26:24 -0400

    testing git with pycharm

--- esoth/media/interfaces.py
+++ esoth/media/interfaces.py
@@ -1,4 +1,42 @@
-from zope.interface import Interface
+from plone.app.z3cform.widget import AjaxSelectFieldWidget
+from plone.autoform import directives
+from plone.namedfile.field import NamedBlobImage
+from plone.supermodel import model
+import zope.schema as schema
 
-class ICDInfo(Interface):
-  """ A cd info interface """
+from esoth.media import _
+
+
+class IAlbumMetadata(model.Schema):
+    """ Metadata for album collection """
+
+    artist = schema.TextLine(
+        title=_(u'Artist'),
+        required=True,
+    )
+
+    country = schema.List(
+        title=_(u'Country'),
+        required=False,
+        value_type=schema.TextLine(),
+    )
+    directives.widget(
+        'subjects',
+        AjaxSelectFieldWidget,
+        vocabulary='esoth.media.Countries'
+    )
+
+    year = schema.Int(
+        title=_(u'Year'),
+        required=False,
+    )
+
+    img_url = schema.URI(
+        title=_(u'Image URL'),
+        required=False,
+    )
+
+    img = NamedBlobImage(
+        title=_(u'Image'),
+        required=False,
+    )
--- setup.py
+++ setup.py
@@ -1,49 +1,18 @@
 # -*- coding: utf-8 -*-
-"""
-This module contains the tool of esoth.media
-"""
 import os
 from setuptools import setup, find_packages
 
-def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
-
 version = '1.0'
-
-long_description = (
-    read('README.txt')
-    + '\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' +
-    read('CHANGES.txt')
-    + '\n' +
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('esoth', 'media', 'README.txt')
-    + '\n' +
-    'Contributors\n'
-    '************\n'
-    + '\n' +
-    read('CONTRIBUTORS.txt')
-    + '\n' +
-    'Download\n'
-    '********\n'
-    )
 
 tests_require=['zope.testing']
 
 setup(name='esoth.media',
       version=version,
       description="CDs, DVDs, etc. info",
-      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-classifiers=[
+      classifiers=[
         'Framework :: Plone',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python'
         ],
       keywords='',
       author='Esoth',
@@ -68,6 +37,5 @@ classifiers=[
       [egg_info.writers]
       paster_plugins.txt = setuptools.command.egg_info:write_arg
       """,
-      paster_plugins = ["ZopeSkel"],
       )
 
